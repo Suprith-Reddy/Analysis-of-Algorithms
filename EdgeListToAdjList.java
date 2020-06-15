f3d755817330b254b8ebaba886fde0e19e1919cd
Suprith-Reddy <suprithgurudu@gmail.com>
2020-06-15 16:57:23 -0400

    Create EdgeListToAdjList.java
    Converting Edge List to Adjacency List for easier coding.
    Example: Edge List - [[0, 1], [1, 2], [3, 4]]    TO    Adj List - 0:(1) ; 1:(0,2) ; 2:(1) ; 3:(4) ; 4:(3)

---
+++ EdgeListToAdjList.java
@@ -0,0 +1,30 @@
+class GraphList{
+    List<Integer> adj;
+    public GraphList(){
+        this.adj = new ArrayList<>();
+    }
+}
+public class EdgeListToAdjList {
+    public static void main(String[] args) {
+        int[][] edges = {{0, 1}, {1, 2}, {3, 4}};
+
+        GraphList[] gl = new GraphList[5];
+        for(int i=0;i<5;i++){
+            gl[i] = new GraphList();
+        }
+
+        for(int i=0;i<edges.length;i++){
+            gl[edges[i][0]].adj.add(edges[i][1]); //Only if Directed Graph is given
+            gl[edges[i][1]].adj.add(edges[i][0]); //Need both instructions if Undirected is given
+        }
+
+        for (GraphList g:
+             gl) {
+            for (Integer n:
+                 g.adj) {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+}
